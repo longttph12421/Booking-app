7a6c3bb046f0c5dd1d2beefc124b112f848a82d1
longttph12421 <longttph12421@fpt.edu.vn>
2021-10-10 23:01:39 +0700

    update connect redux
    update connect redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Components from "./views/Components/Components";
 import LandingPage from "./views/LandingPage/LandingPage";
 import ProfilePage from "./views/ProfilePage/ProfilePage";
 import LoginPage from "./views/LoginPage/LoginPage";
-import { Route, Switch } from "react-router-dom";
+import ProductBox from "./container/product/ProductBox";
 function App() {
   return (
     <div>
-       <Switch>
+      <Switch>
         <Route path="/landing-page" component={LandingPage} />
         <Route path="/profile-page" component={ProfilePage} />
         <Route path="/login-page" component={LoginPage} />
+        <Route path="/product" component={ProductBox} />
         <Route path="/" component={Components} />
       </Switch>
     </div>
